211c24eac8c806ee5c479732f775e985a06af7a7
Sergey <sergejbogacev20@gmail.com>
2020-02-09 19:48:46 +0500

    таблица без ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,20 @@
 import { Component } from '@angular/core';
 
+type man = {id: string, fname: string, lname: string, mname: string};
+
+/* class man {
+  id: string
+  fname: string
+  lname: string
+  mname: string
+
+  constructor(id: string, fname: string, lname: string, mname: string) {
+    this.id = id
+    this.fname = fname
+    this.lname = lname
+    this.mname = mname
+  } */
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +22,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'wings';
+  head = [
+    [['Номер','id'],['ФИО','Name']],
+    [['Имя','firstName'],['Фамилия','lastName'],['Отчество','middleName']]
+  ];
+  people: man[] = [
+    {id : '1', fname : 'Николай', lname : 'Васильевич', mname : 'Куссмауль'},
+    {id : '2', fname : 'Семен', lname : 'Львович', mname : 'Шредингер'},
+    {id : '3', fname : 'Алексей', lname : 'Никанорович', mname : 'Цукерберг'}
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from  '@angular/forms';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
@@ -18,7 +18,8 @@ import {MatTableModule} from '@angular/material/table';
     MatButtonModule,
     MatIconModule,
     MatTabsModule,
-    MatTableModule
+    MatTableModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
